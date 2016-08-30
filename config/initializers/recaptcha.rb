ceac02fedc5679c1b40ef77e34be55b3f88b1662
bhaskar <bhaskar@murabahaclub.com>
2016-08-30 17:53:13 +0530

    Google captcha API

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Ld04ygTAAAAAJ5SaU0TUUuyC4fq_QQ434ihky83'
+  config.private_key = '6Ld04ygTAAAAAE3P6R9nnBhZ-QO5hqQNSPqE28yn'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
